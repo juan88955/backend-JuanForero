4b8ae30e828802418b14ebfdbf629ba336689c89
Juan Carlos Forero <juan.forero66@gmail.com>
2024-10-15 21:44:37 -0500

    trabajo en clase

--- router/users.js
+++ router/users.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import {allUsers, UserByRole} from '../controllers/users/read.js';
+import {create} from '../controllers/users/create.js';
 
 const router = Router()
 
 router.get('/all', allUsers)
 router.get('/role/:x', UserByRole)
+router.post('/create', create)
+
 
 
 export default router
